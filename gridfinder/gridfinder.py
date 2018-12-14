f053e266fcd9f6d030584a4b65f7ff395d48bbf4
Chris Arderne <chris@rdrn.me>
2018-12-14 17:12:42 +0100

    New prev mode use direction instead of location, slower but less memory?

--- gridfinder/gridfinder.py
+++ gridfinder/gridfinder.py
@@ -40,7 +40,7 @@ def get_costs(costs_in):
     return costs
 
 
-def optimise_generator(targets, costs, start):
+def optimise_generator(targets, costs, start, display_progress=False, use_direction=True):
     """
 
     """
@@ -54,7 +54,11 @@ def optimise_generator(targets, costs, start):
     
     visited = np.zeros_like(costs)
     dist = np.full_like(costs, np.nan)
-    prev = np.full_like(costs, np.nan, dtype=object)
+    if use_direction:
+        prev = np.full_like(costs, np.nan)
+    else:
+        prev = np.full_like(costs, np.nan, dtype=object)
+
     dist[start] = 0
     
     #       dist, loc
@@ -67,11 +71,16 @@ def optimise_generator(targets, costs, start):
             visited[loc] = 1
             heappush(halo, [0, loc])
 
-            prev_loc = prev[loc]
+            if use_direction:
+                prev_loc = get_location(at=loc, direction=prev[loc])
+            else:
+                prev_loc = prev[loc]
+
             if type(prev_loc) == tuple:
                 zero_and_heap_path(prev_loc)
     
-    handle = display(Markdown(''), display_id=True)
+    if display_progress:
+        handle = display(Markdown(''), display_id=True)
     while len(halo):
         current = heappop(halo)       
         current_loc = current[1]
@@ -99,10 +108,16 @@ def optimise_generator(targets, costs, start):
                 # skip if we've already set dist to 0
                 if dist[next_loc] == 0:
                     continue
+
+                if use_direction:
+                    dir_prev = get_direction(at=next_loc, to=current_loc)
                 
                 # if the location is connected
                 if targets[next_loc]:
-                    prev[next_loc] = current_loc
+                    if use_direction:
+                        prev[next_loc] = dir_prev
+                    else:
+                        prev[next_loc] = current_loc
                     zero_and_heap_path(next_loc)
                     #print('FOUND CONNECTED at', next_loc)
                 
@@ -120,7 +135,10 @@ def optimise_generator(targets, costs, start):
                         if next_dist < dist[next_loc]:
                             #print('REVISITING at', next_loc, '  NEW DIST', next_dist)
                             dist[next_loc] = next_dist
-                            prev[next_loc] = current_loc
+                            if use_direction:
+                                prev[next_loc] = dir_prev
+                            else:
+                                prev[next_loc] = current_loc
                             heappush(halo, [next_dist, next_loc])
 
                     else:
@@ -132,11 +150,15 @@ def optimise_generator(targets, costs, start):
                         heappush(halo, [next_dist, next_loc])
                         visited[next_loc] = 1
                         dist[next_loc] = next_dist
-                        prev[next_loc] = current_loc
+                        if use_direction:
+                            prev[next_loc] = dir_prev
+                        else:
+                            prev[next_loc] = current_loc
 
                         if int(progress_new) > int(progress):
                             progress = progress_new
-                            handle.update(f'{progress:.2f}%')
+                            if display_progress:
+                                handle.update(f'{progress:.2f}%')
 
                             yield np.copy(dist)
                         
@@ -146,12 +168,12 @@ def optimise_generator(targets, costs, start):
     yield np.copy(dist)
 
 
-def optimise(targets, costs, start):
+def optimise(targets, costs, start, display_progress=False, use_direction=True):
     """
 
     """
 
-    generator = optimise_generator(targets, costs, start)
+    generator = optimise_generator(targets, costs, start, display_progress)
 
     dist = None
     for dist in generator:
@@ -159,3 +181,76 @@ def optimise(targets, costs, start):
 
     return dist
 
+
+def get_direction(at=None, to=None):
+    """
+    Direction based on this: (H is where we're at)
+    5  4  3
+    6  H  2
+    7  0  1
+    """
+
+    at_x = at[1]
+    at_y = at[0]
+    
+    to_x = to[1]
+    to_y = to[0]
+    
+    diff_x = to_x - at_x
+    diff_y = to_y - at_y
+    
+    if diff_x == 0 and diff_y == 1:
+        return 0
+    elif diff_x == 1 and diff_y == 1:
+        return 1
+    elif diff_x == 1 and diff_y == 0:
+        return 2
+    elif diff_x == 1 and diff_y == -1:
+        return 3
+    elif diff_x == 0 and diff_y == -1:
+        return 4
+    elif diff_x == -1 and diff_y == -1:
+        return 5
+    elif diff_x == -1 and diff_y == 0:
+        return 6
+    elif diff_x == -1 and diff_y == 1:
+        return 7
+
+
+def get_location(at=None, direction=None):
+    """
+    Direction based on this: (H is where we're at)
+    5  4  3
+    6  H  2
+    7  0  1
+    """
+
+    diff_x = None
+    diff_y = None
+    
+    if direction == 0:
+        diff_x = 0
+        diff_y = 1
+    elif direction == 1:
+        diff_x = 1
+        diff_y = 1
+    elif direction == 2:
+        diff_x = 1
+        diff_y = 0
+    elif direction == 3:
+        diff_x = 1
+        diff_y = -1
+    elif direction == 4:
+        diff_x = 0
+        diff_y = -1
+    elif direction == 5:
+        diff_x = -1
+        diff_y = -1
+    elif direction == 6:
+        diff_x = -1
+        diff_y = 0
+    elif direction == 7:
+        diff_x = -1
+        diff_y = 1
+        
+    return (at[0] + diff_y, at[1] + diff_x)
