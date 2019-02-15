4fbe21cb80a14a42f72bc4e4579847d1392cd46b
Chris Arderne <chris@rdrn.me>
2019-02-15 12:58:06 +0100

    Replace Makefile with test.sh

---
+++ test.sh
@@ -0,0 +1,23 @@
+# test.sh
+#!/bin/bash
+
+# clean output folder
+rm -r test_output || true
+
+# convert the example notebook to a script
+jupyter nbconvert --to script example.ipynb
+
+# and disable Jupyter display handle output
+sed -i -e 's/jupyter=True/jupyter=False/g' example.py
+
+# activate virtualenv if present but continue anyway
+source /home/chris/.envs/gridfinder/bin/activate || true
+
+# for rasterio/Fiona CRS issues
+export GDAL_DATA="/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/fiona/gdal_data"
+
+# run script
+python3 example.py
+
+# clean up
+rm example.py
