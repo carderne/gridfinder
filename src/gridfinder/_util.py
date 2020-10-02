3c2e06a28360e67b75c6d3a6e1b81d173d6484a3
Michael Panchenko <michael.panchenko@unternehmertum.de>
2020-10-02 11:58:03 +0200

    Removed previous clipping and saving methods

--- src/gridfinder/_util.py
+++ src/gridfinder/_util.py
@@ -7,57 +7,9 @@ Functions:
 - clip_line_poly
 - clip_raster
 """
-import json
 import logging
-from pathlib import Path
-
-import geopandas as gpd
-import rasterio
-from rasterio.mask import mask
 
 log = logging.getLogger(__name__)
-
-
-def save_raster(path, raster, affine, crs=None, nodata=0):
-    """Save a raster to the specified file.
-
-    Parameters
-    ----------
-    path : str
-        Output file path
-    raster : numpy.array
-        2D numpy array containing raster values
-    affine: affine.Affine
-        Affine transformation for the raster
-    crs: str, proj.Proj, optional (default EPSG4326)
-        CRS for the raster
-    """
-
-    path = Path(path)
-    if not path.parents[0].exists():
-        path.parents[0].mkdir(parents=True, exist_ok=True)
-
-    if path.exists():
-        log.warning(f"Deleting {path}")
-        path.unlink()
-
-    if not crs:
-        crs = "+proj=latlong"
-
-    filtered_out = rasterio.open(
-        path,
-        "w",
-        driver="GTiff",
-        height=raster.shape[0],
-        width=raster.shape[1],
-        count=1,
-        dtype=raster.dtype,
-        crs=crs,
-        transform=affine,
-        nodata=nodata,
-    )
-    filtered_out.write(raster, 1)
-    filtered_out.close()
 
 
 def clip_line_poly(line, clip_poly):
@@ -94,61 +46,3 @@ def clip_line_poly(line, clip_poly):
     clipped = clipped[~clipped.is_empty]
 
     return clipped
-
-
-# clip_raster is copied from openelec.clustering
-def clip_raster(raster, boundary, boundary_layer=None):
-    """Clip the raster to the given administrative boundary.
-
-    Parameters
-    ----------
-    raster : string, pathlib.Path or rasterio.io.DataSetReader
-        Location of or already opened raster.
-    boundary : string, pathlib.Path or geopandas.GeoDataFrame
-        The polygon by which to clip the raster.
-    boundary_layer : string, optional
-        For multi-layer files (like GeoPackage), specify the layer to be used.
-
-
-    Returns
-    -------
-    tuple
-        Three elements:
-            clipped : numpy.ndarray
-                Contents of clipped raster.
-            affine : affine.Affine()
-                Information for mapping pixel coordinates
-                to a coordinate system.
-            crs : dict
-                Dict of the form {'init': 'epsg:4326'} defining the coordinate
-                reference system of the raster.
-
-    """
-
-    if isinstance(raster, Path):
-        raster = str(raster)
-    if isinstance(raster, str):
-        raster = rasterio.open(raster)
-
-    if isinstance(boundary, Path):
-        boundary = str(boundary)
-    if isinstance(boundary, str):
-        if ".gpkg" in boundary:
-            driver = "GPKG"
-        else:
-            driver = None  # default to shapefile
-            boundary_layer = ""  # because shapefiles have no layers
-
-        boundary = gpd.read_file(boundary, layer=boundary_layer, driver=driver)
-
-    if not (boundary.crs == raster.crs or boundary.crs == raster.crs.data):
-        boundary = boundary.to_crs(crs=raster.crs)
-    coords = [json.loads(boundary.to_json())["features"][0]["geometry"]]
-
-    # mask/clip the raster using rasterio.mask
-    clipped, affine = mask(dataset=raster, shapes=coords, crop=True)
-
-    if len(clipped.shape) >= 3:
-        clipped = clipped[0]
-
-    return clipped, affine, raster.crs
