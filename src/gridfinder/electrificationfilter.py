fd1badb006113f8774ebbb18897e839d4d334084
Sebastian Wagner <sebastian.wagner@unternehmertum.de>
2020-10-02 15:56:09 +0200

    Improve docstring.

--- src/gridfinder/electrificationfilter.py
+++ src/gridfinder/electrificationfilter.py
@@ -31,9 +31,9 @@ class NightlightFilter(ElectrificationFilter):
         The goal of this filter is to find pixels with a higher value than their neighborhood,
         biased towards closer areas. To achieve this bias, a non-linear function is needed,
         and a cubic function was found to achieve better results than a square function.
-        The filter is normalized so that and the output is then subtracted from the original image.
-        :param i: row number
-        :param j: column number
+
+        :param i: row number with respect to filter height
+        :param j: column number with respect to filter width
         :return: Value
         """
         d_rows = abs(i - math.floor(self.shape[0] / 2))
@@ -48,7 +48,8 @@ class NightlightFilter(ElectrificationFilter):
     def _func_to_filter(self):
         """
         Construct a filter from the pixel-wise function.
-        :return:
+        The filter is normalized so that and the output is then subtracted from the original image.
+        :return: Numpy array with filter values
         """
         vec_filter_func = np.vectorize(self._filter_func)
         ntl_filter = np.fromfunction(vec_filter_func, self.shape, dtype=float)
