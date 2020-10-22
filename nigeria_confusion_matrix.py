19c97cd84ec0f1b588e6a73d4ff256ba9f283697
Sebastian Wagner <sebastian.wagner@unternehmertum.de>
2020-10-22 11:50:36 +0200

    Add confusion matrix.

---
+++ nigeria_confusion_matrix.py
@@ -0,0 +1,100 @@
+import sys, os
+
+# in order to get the config, it is not part of the library
+# os.chdir("..")
+# sys.path.append(os.path.abspath("."))
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from matplotlib import cm
+import seaborn as sns
+import rasterio
+import numpy as np
+
+import folium
+
+from gridfinder.gridfinder import optimise, estimate_mem_use, get_targets_costs
+from gridfinder.post import raster_to_lines, thin, threshold_distances, accuracy
+from gridfinder.util.raster import save_2d_array_as_raster, get_clipped_data
+from gridfinder.util.loading import open_raster_in_tar
+from gridfinder.prepare import merge_rasters, drop_zero_pop, prepare_ntl, prepare_roads
+from gridfinder.electrificationfilter import NightlightFilter
+from gridfinder.metrics import eval_confusion_matrix
+from config import get_config
+from data_access.remote_storage import RemoteStorage, RemoteStorageConfig
+
+c = get_config(reload=True)
+folder_ntl_in = c.datafile_path("nightlight_imagery/75N060W", stage=c.RAW)
+aoi_in = c.datafile_path("nigeria-kano.geojson", stage=c.GROUND_TRUTH)
+roads_in = c.datafile_path("nigeria-roads-200101.gpkg", stage=c.GROUND_TRUTH)
+pop_in = c.datafile_path("population_nga_2018-10-01.tif", stage=c.GROUND_TRUTH)
+grid_truth = c.datafile_path("grid-files/nigeria.gpkg", stage=c.GROUND_TRUTH)
+
+folder_ntl_out = c.datafile_path(
+    "ntl_nigeria_clipped", stage=c.PROCESSED, check_existence=False
+)
+raster_merged_out = c.datafile_path(
+    "ntl_nigeria_merged.tif", stage=c.PROCESSED, check_existence=False
+)
+targets_out = c.datafile_path(
+    "nigeria_targets.tif", stage=c.PROCESSED, check_existence=False
+)
+targets_clean_out = c.datafile_path(
+    "nigeria_targets_clean.tif", stage=c.CLEANED, check_existence=False
+)
+roads_out = c.datafile_path(
+    "nigeria_roads.tif", stage=c.PROCESSED, check_existence=False
+)
+
+dist_out = c.datafile_path("nigeria_dist.tif", stage=c.PROCESSED, check_existence=False)
+guess_out = c.datafile_path(
+    "nigeria_guess.tif", stage=c.PROCESSED, check_existence=False
+)
+guess_skeletonized_out = c.datafile_path(
+    "nigeria_guess_skel.tif", stage=c.PROCESSED, check_existence=False
+)
+guess_nulled = c.datafile_path(
+    "nigeria_guess_nulled.tif", stage=c.PROCESSED, check_existence=False
+)
+guess_vec_out = c.datafile_path(
+    "nigeria_guess.gpkg", stage=c.PROCESSED, check_existence=False
+)
+animate_out = os.path.join(c.visualizations, "nigeria_guess_viz.tif")
+
+
+df = gpd.read_file(grid_truth)
+prediction = rasterio.open(guess_out)  #
+aoi = gpd.read_file(aoi_in)
+print(grid_truth, guess_out, aoi_in)
+print("Cell size at raster resolution (ca. 220m)")
+confusion = eval_confusion_matrix(df, prediction, aoi=aoi)
+print(confusion)
+accuracy = (confusion.tp + confusion.tn) / (
+    confusion.tp + confusion.tn + confusion.fp + confusion.fn
+)
+precision = (confusion.tp) / (confusion.tp + confusion.fp)
+recall = (confusion.tp) / (confusion.tp + confusion.fn)
+print("accuracy: ", accuracy, "precision: ", precision, "recall: ", recall)
+print("New cell size 15km")
+confusion = eval_confusion_matrix(df, prediction, cell_size_in_meters=15000, aoi=aoi)
+print(confusion)
+accuracy = (confusion.tp + confusion.tn) / (
+    confusion.tp + confusion.tn + confusion.fp + confusion.fn
+)
+precision = (confusion.tp) / (confusion.tp + confusion.fp)
+recall = (confusion.tp) / (confusion.tp + confusion.fn)
+print("accuracy: ", accuracy, "precision: ", precision, "recall: ", recall)
+
+print("New cell size 10km")
+confusion = eval_confusion_matrix(df, prediction, cell_size_in_meters=10000, aoi=aoi)
+print(confusion)
+accuracy = (confusion.tp + confusion.tn) / (
+    confusion.tp + confusion.tn + confusion.fp + confusion.fn
+)
+precision = (confusion.tp) / (confusion.tp + confusion.fp)
+recall = (confusion.tp) / (confusion.tp + confusion.fn)
+print("accuracy: ", accuracy, "precision: ", precision, "recall: ", recall)
