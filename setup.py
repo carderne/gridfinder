19669c042d80f9b6359c46078ac2a3337bd1d4af
Sebastian Lettner <sebastian.lettner@unternehmertum.de>
2020-10-23 14:55:57 +0200

    changed version in setup py from dev4 to dev3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    version="1.2.0-dev4",
+    version="1.2.0-dev3",
     description="Library for finding gridlines from nightlight imagery",
     install_requires=open("requirements.txt").readlines()[1:],
     dependency_links=["https://nexus.admin.aai.sh/repository/aai-pypi/simple"],
