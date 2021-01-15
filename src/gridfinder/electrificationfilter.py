bb7dfb26d58273e62143f7d1b2239ba75171413f
Sebastian Wagner <sebastian.wagner@unternehmertum.de>
2021-01-15 17:55:29 +0100

    Refactored weights function.

--- src/gridfinder/electrificationfilter.py
+++ src/gridfinder/electrificationfilter.py
@@ -5,37 +5,37 @@ import numpy as np
 from scipy import signal
 from mullet.filters.models import TorchImageFilter, Conv2dImageFilter
 from torch import Tensor, sub, add
+from typing import Tuple
 
 
-def _filter_func(self, i: int, j: int):
-    """
-    d is that pixel’s perpendicular distance from a given square sample’s centroid.
-    The goal of this predictor is to find pixels with a higher value than their neighborhood,
-    biased towards closer areas. To achieve this bias, a non-linear function is needed,
-    and a cubic function was found to achieve better results than a square function.
-
-    :param i: row number with respect to predictor height
-    :param j: column number with respect to predictor width
-    :return: Value
-    """
-    d_rows = abs(i - math.floor(self.shape[0] / 2))
-    d_cols = abs(j - math.floor(self.shape[1] / 2))
-    d = sqrt(d_rows ** 2 + d_cols ** 2)
-
-    if d == 0:
-        return 0.0
-    else:
-        return 1 / (1 + d / 2) ** 3
-
-
-def _func_to_filter(self):
+def get_weights_array(shape: Tuple[int, int], i: int, j: int):
     """
     Construct a predictor from the pixel-wise function.
     The predictor is normalized so that and the output is then subtracted from the original image.
+    :param i: row number with respect to predictor height
+    :param j: column number with respect to predictor width
+    :param shape: shape of filter
     :return: Numpy array with predictor values
     """
-    vec_filter_func = np.vectorize(self._filter_func)
-    ntl_filter = np.fromfunction(vec_filter_func, self.shape, dtype=float)
+
+    def _filter_func(i: int, j: int):
+        """
+        d is that pixel’s perpendicular distance from a given square sample’s centroid.
+        The goal of this predictor is to find pixels with a higher value than their neighborhood,
+        biased towards closer areas. To achieve this bias, a non-linear function is needed,
+        and a cubic function was found to achieve better results than a square function.
+        """
+        d_rows = abs(i - math.floor(shape[0] / 2))
+        d_cols = abs(j - math.floor(shape[1] / 2))
+        d = sqrt(d_rows ** 2 + d_cols ** 2)
+
+        if d == 0:
+            return 0.0
+        else:
+            return 1 / (1 + d / 2) ** 3
+
+    vec_filter_func = np.vectorize(_filter_func())
+    ntl_filter = np.fromfunction(vec_filter_func, shape, dtype=float)
     return ntl_filter / ntl_filter.sum()
 
 
@@ -57,7 +57,7 @@ class NightlightFilter(ElectrificationFilter):
         :param shape: Shape of predictor, e.g. (2,2)
         """
         self.shape = shape
-        self.predictor = _func_to_filter()
+        self.predictor = get_weights_array(shape)
 
     def predict(self, data: np.ndarray):
         """
@@ -78,11 +78,11 @@ class NightlightTorchFilter(Conv2dImageFilter):
     """
 
     def __init__(
-        self, init_weights=_func_to_filter(), radius=21, bias=False, threshold=0.4
+        self, init_weights=get_weights_array, radius=21, bias=False, threshold=0.4
     ):
         super().__init__(radius=radius, bias=bias, padding_mode="reflect")
         self.threshold = threshold
-        self._set_weight(init_weights)
+        self._set_weight(init_weights((self.kernel_size, self.kernel_size)))
 
     def _forward(self, x: Tensor) -> Tensor:
         x = x.unsqueeze(0).unsqueeze(0)
